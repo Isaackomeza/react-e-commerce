3e7d912e8db90eddbc3a08ee7fe5227be4fdacef
Isaac <isaackomeza@gmail.com>
2020-12-18 16:17:27 +0200

    updating stripe integration and updating favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51HziHcFhgxZPiyWsfuY60Afd9RPC5gas776ZVhkU8oIGJpBwDTljNsxqL8QjPeCBcJzeQXI5RJLFKLUVHJecUexn0054feuWro';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='Clothing E-commerce Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;

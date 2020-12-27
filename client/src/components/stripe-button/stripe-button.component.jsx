6c13d6044528fb0f7353b980d0ec429337f9bd3a
Isaac <isaackomeza@gmail.com>
2020-12-27 21:37:10 +0200

    Separated client with server and added server endpoint for stripe

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_51HziHcFhgxZPiyWsfuY60Afd9RPC5gas776ZVhkU8oIGJpBwDTljNsxqL8QjPeCBcJzeQXI5RJLFKLUVHJecUexn0054feuWro';
 
   const onToken = token => {
-    console.log(token);
-    alert('Payment Succesful!');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token: token
+      }
+    })
+      .then(response => {
+        alert('succesful payment');
+      })
+      .catch(error => {
+        console.log('Payment Error: ', error);
+        alert(
+          'There was an issue with your payment! Please make sure you use the provided credit card.'
+        );
+      });
   };
 
   return (
